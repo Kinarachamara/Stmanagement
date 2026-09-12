74fe1ec9ed949b8d68c15867fdbb64eae9ea89bb
Kinarachamara <125230664+Kinarachamara@users.noreply.github.com>
2026-09-12 20:12:12 +0530

    Change link type for IRC Paper No. 06 to PDF
    Updated the link for IRC Paper No. 06 to point to the PDF version.

--- data.js
+++ data.js
@@ -163,7 +163,7 @@ const classData = [
           {
             id: "ictp-06", num: "06", title: "IRC Paper No. 06",
             links: [
-              { type: "pending", label: "📄 IRC Paper No. 06 – Pending" },
+              { type: "pdf", label: "📄 IRC Paper No. 06" url:"https://drive.google.com/file/d/1BywRsxQWSyCj1Pqp-rgg18448QapZ3Ul/view?usp=drive_link" },
               { type: "pending", label: "▶ Discussion – Pending" },
               { type: "pending", label: "✅ Marking – Pending" }
             ]
